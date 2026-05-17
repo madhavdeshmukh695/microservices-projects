2e20ba876b1ebc26654deeafcdba45b469378f59
Madhav Deshmukh <148607099+madhavdeshmukh695@users.noreply.github.com>
2026-05-17 08:52:32 +0530

    Delete section12 directory

--- section12/accounts/src/main/java/com/eazybytes/accounts/service/ICustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.eazybytes.accounts.service;
-
-import com.eazybytes.accounts.dto.CustomersDetailsDto;
-
-public interface ICustomerService {
-
-    CustomersDetailsDto fetchCustomersDetails(String mobileNumber, String correlationId);
-}
